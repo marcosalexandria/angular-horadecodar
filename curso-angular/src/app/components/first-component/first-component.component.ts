6dfa1797045f82e1dc8f0125c817c1f90cbcb589
Marcos Alexandria <marcos.alexandria01@gmail.com>
2024-10-22 14:53:33 -0300

    Curso Angular 13: Dados no template - #05

--- curso-angular/src/app/components/first-component/first-component.component.ts
+++ curso-angular/src/app/components/first-component/first-component.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-first-component',
   templateUrl: './first-component.component.html',
   styleUrl: './first-component.component.css'
 })
-export class FirstComponentComponent {
+export class FirstComponentComponent implements OnInit{
+  nome: string = "Marcos";
+  idade: number = 27;
+  passaTempo = ["Ler", "Estudar"];
+  carro = {
+    modelo: "Polo",
+    ano: 2020
+  }
 
+  constructor(){}
+
+  ngOnInit(): void{}
 }
